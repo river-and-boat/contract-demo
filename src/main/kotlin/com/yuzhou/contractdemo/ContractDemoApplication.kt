4428fef0448a76318cbb7cf83e193cf89f458eda
yuzhou.jiang <yuzhou.jiang@tw.com>
2023-07-07 11:35:59 +0800

    init project

---
+++ src/main/kotlin/com/yuzhou/contractdemo/ContractDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.yuzhou.contractdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContractDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ContractDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/yuzhou/contractdemo/ContractDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yuzhou.contractdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContractDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
